1541c5a5ee65d417f98bee3075d9df342ac5f9da
Alexandre Preczewski <apreczewski@gmail.com>
2024-06-04 22:51:51 -0300

    update

--- index.py
+++ index.py
@@ -2,10 +2,16 @@ from flask import Flask, jsonify
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return jsonify({ test: "blabla"})
 
-@app.route('/hello')
-def hello():
-    return 'Hello, Musica Cafe'
+@app.route('/data', methods=['GET'])
+def get_data():
+    # Aqui você pode definir seu JSON de exemplo ou buscar os dados de outra fonte
+    data = {
+        "nome": "Exemplo",
+        "idade": 30,
+        "cidade": "São Paulo"
+    }
+    return jsonify(data)
+
+if __name__ == '__main__':
+    app.run(debug=True)
